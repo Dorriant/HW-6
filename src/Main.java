228f09395195397c0a8c877302e2bc51155cafa5
Dorriant <barakrusa@gmail.com>
2024-12-15 11:19:12 +0700

    Error correction

--- src/Main.java
+++ src/Main.java
@@ -2,46 +2,47 @@ public class Main {
     public static void main(String[] args) {
         // Циклы
 // Задача 1
-        for (int i = 0; i < 10; i++){
-            System.out.println(i);
+        for (int i = 1; i <= 10; i++){
+            System.out.print(i + " ");
         }
-
+        System.out.println();
 // Задача 2
         for (int i = 10; i <= 10 && i >= 1; i = i - 1){
-            System.out.println(i);
+            System.out.print(i + " ");
         }
-
+        System.out.println();
 // Задача 3
-        for (int i = 0; i < 17; i = i +2){
-            System.out.println(i);
+        for (int i = 0; i <= 17; i = i +2){
+            System.out.print(i + " ");
         }
-
+        System.out.println();
 // Задача 4
         for (int i = 10; i <=10 && i >= -10; i = i - 1){
-            System.out.println(i);
+            System.out.print(i + " ");
         }
-
+        System.out.println();
 // Задача 5
         for (int i = 1904; i <=2096; i = i + 4){
-            System.out.println(i);
+            System.out.println(i + " год считается високосным.");
         }
 
 // Задача 6
         for (int i = 7; i >=7 && i < 100; i = i + 7){
-            System.out.println(i);
+            System.out.print(i + " ");
         }
-
+        System.out.println();
 // Задача 7
         for (int i = 1; i >= 1 && i <= 512; i = i * 2){
-            System.out.println(i);
+            System.out.print(i + " ");
         }
-
+        System.out.println();
 // Задача 8
         int salary = 29000;
         int total = 0;
 
         for (int i = 0; i < 12; i++){
             total = total + salary;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей.");
         }
         System.out.println(total);
 
